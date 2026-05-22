924f8145c016d90b5e05fee4058296b4c9c16810
Churchil Yash Rajpal <85837940+churchil15@users.noreply.github.com>
2026-05-23 00:19:36 +0530

    Update EarthquakeLoader.java

--- app/src/main/java/com/example/quakereport/EarthquakeLoader.java
+++ app/src/main/java/com/example/quakereport/EarthquakeLoader.java
@@ -10,10 +10,7 @@ public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
 
     private static final String LOG_TAG = EarthquakeLoader.class.getName();
 
-    // Query URL
     private String url;
-
-    // Constructs a new EarthquakeLoader
     public EarthquakeLoader(Context context, String u){
         super(context);
         url = u;
@@ -24,14 +21,12 @@ public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
         forceLoad();
     }
 
-    // This is on a background thread.
     @Override
     public List<Earthquake> loadInBackground() {
         if (url == null){
             return null;
         }
 
-        // Perform the network request, parse the response, and extract a list of earthquakes.
         List<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData((url));
         return earthquakes;
     }
